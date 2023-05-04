e14f3807bb956005bf0bcf699e9cb1c839c8114c
pva18 <petervarga0018@gmail.com>
2023-05-05 00:16:56 +0200

    Add cource code

---
+++ BeleptetoRendszer_Kozponti/BeleptetoRendszer_Kozponti.ino
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+    Serial.println();
+    Serial.println("Belepteto rendszer kozponti");
+    Serial.println();
+}
+
+void loop()
+{
+}
